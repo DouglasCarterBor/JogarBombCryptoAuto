ca89f22a66bb92211f439d0565e2891fb56bc9d3
douglascarteribordignon <douglasbordignon8@gmail.com>
2022-01-05 18:08:46 -0300

    Ajuste em interface_grafica

--- interface_grafica.py
+++ interface_grafica.py
@@ -1,10 +1,17 @@
 import requests
 from tkinter import *
+
+import jogar_bomb
 from jogar_bomb import iniciar_bomb
+
 
 janela = Tk()
 janela.title("Jogar Bomb Crypto")
+janela.geometry("400x400")
 texto_orientacao = Label(janela, text="Clique para iniciar o game")
+texto_orientacao.grid(column=0, row=0, padx=10, ipady=10)
 botao = Button(janela, text="Iniciar Bomb", command=iniciar_bomb)
-botao.grid(column=0, row=3, padx=10, ipady=10)
+botao.grid(column=0, row=1, padx=10, ipady=10)
+botao = Button(janela, text="Finalizar Bomb", command=jogar_bomb.finalizar_bomb)
+botao.grid(column=0, row=2, padx=10, ipady=10)
 janela.mainloop()
--- jogar_bomb.py
+++ jogar_bomb.py
@@ -17,16 +17,16 @@ def iniciar_bomb():
     pyautogui.press('f11')
     # 04 - CLICAR EM CONECTAR CARTEIRA
     time.sleep(8)
-    pyautogui.moveTo(990, 789)
+    pyautogui.moveTo(954, 668)
     time.sleep(0.1)
     pyautogui.click()
     # 05 - ASSINAR
     time.sleep(8)
-    pyautogui.moveTo(1806, 696)
+    pyautogui.moveTo(1782, 696)
     pyautogui.click()
     # 06 - CLICAR EM CAÇA TESOURO
     time.sleep(20)
-    pyautogui.moveTo(981, 552)
+    pyautogui.moveTo(959, 446)
     pyautogui.click()
     i = 0
     x = 0
@@ -35,33 +35,25 @@ def iniciar_bomb():
         x += 1
         # 07 - MOSTRAR HERÓIS
         time.sleep(1)
-        pyautogui.moveTo(974, 894)
+        pyautogui.moveTo(951, 785)
         pyautogui.click()
         # 08 - CLICAR EM HERÓIS
         time.sleep(1)
-        pyautogui.moveTo(970, 864)
+        pyautogui.moveTo(949, 762)
         pyautogui.click()
-        # 09 - COLOCAR 01 PARA TRABALHAR
-        time.sleep(10)
-        pyautogui.moveTo(871, 395)
-        pyautogui.click()
-        # 10 - COLOCAR 02 PARA TRABALHAR
+        # 09 - COLOCAR HERÓIS PARA TRABALHAR
         time.sleep(1)
-        pyautogui.moveTo(865, 490)
+        pyautogui.moveTo(848, 271)
         pyautogui.click()
-        # 11 - COLOCAR 03 PARA TRABALHAR
-        time.sleep(1)
-        pyautogui.moveTo(871, 580)
-        pyautogui.click()
-        # 12 - FECHAR
+        # 10 - FECHAR
         time.sleep(0.1)
-        pyautogui.moveTo(1046, 312)
+        pyautogui.moveTo(1025, 209)
         pyautogui.click()
-        # 13 - BAIXAR ABA
+        # 11 - BAIXAR ABA
         time.sleep(1)
-        pyautogui.moveTo(974, 755)
+        pyautogui.moveTo(950, 679)
         pyautogui.click()
-        # 14 - ESPERAR O TRABALHO
+        # 12 - ESPERAR O TRABALHO
         time.sleep(500)
         while x == 6 or x == 13 or x == 20 or x == 30 or x == 40 or x == 50:
             x += 1
@@ -82,5 +74,7 @@ def iniciar_bomb():
             pyautogui.moveTo(981, 552)
             pyautogui.click()
 
-    pyautogui.alert("O código acabou de rodar, pode utilizar seu computador de novo!")
+    #pyautogui.alert("O código acabou de rodar, pode utilizar seu computador de novo!")
+def finalizar_bomb():
+    exit(iniciar_bomb)
 
